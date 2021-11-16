dd15b24c00e926a4b3a562a5401076566150a1ca
Filippo <41006+filoa86@users.noreply.github.com>
2021-11-16 13:02:08 +0100

    Update backup-restore-database.ps1

--- Powershell/backup-restore-database.ps1
+++ Powershell/backup-restore-database.ps1
@@ -5,12 +5,21 @@ Param(
   )]
   [string]$DbName,
   [Parameter(
-      HelpMessage = "Execute sql database restore without performing backup"
+      HelpMessage = "Execute SQL database restore"
   )]
-  [switch]$Restore  
+  [switch]$Restore
+  [Parameter(
+      HelpMessage = "Execute SQL database backup"
+  )]
+  [switch]$Backup
 )
 
 Clear-Host
+
+if(!$Backup -And ! $Restore) {
+  Write-Error "-Backup or -Restore parameters are mandatory"
+  Exit 1
+}
 
 $server = "localhost"
 $backupPath = "c:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\"
@@ -31,33 +40,35 @@ $credJson = (Get-Content -Raw -Path $configJson | ConvertFrom-Json)
 $cred = New-Object -TypeName System.Management.Automation.PSCredential -Argumentlist "sa", ($credJson.password | ConvertTo-SecureString)
 
 $bakFullPath = "$backupPath\$DbName.bak"
-if(!$Restore) {
-    Write-Host "Backup database $DbName..." -NoNewline    
-    if(Test-Path $bakFullPath) {        
-        Remove-Item $bakFullPath -Force    
-    }
-    Backup-SqlDatabase -ServerInstance $server -Database $DbName -Credential $cred -CompressionOption On -Initialize
-    Write-Host "[OK]"
+if($Backup) {
+  Write-Host "Backup database $DbName..." -NoNewline    
+  if(Test-Path $bakFullPath) {        
+      Remove-Item $bakFullPath -Force    
+  }
+  Backup-SqlDatabase -ServerInstance $server -Database $DbName -Credential $cred -CompressionOption On -Initialize
+  Write-Host "[OK]"
 }
 
-$srv = New-Object ('Microsoft.SqlServer.Management.Smo.Server')
-$rs = New-Object('Microsoft.SqlServer.Management.Smo.Restore')
-$bdi = New-Object ('Microsoft.SqlServer.Management.Smo.BackupDeviceItem') ($bakFullPath, 'File')
-$rs.Devices.Add($bdi)
-$fl = $rs.ReadFileList($srv)
-$logname = $fl.logicalname 
+if($Restore) {
+  $srv = New-Object ('Microsoft.SqlServer.Management.Smo.Server')
+  $rs = New-Object('Microsoft.SqlServer.Management.Smo.Restore')
+  $bdi = New-Object ('Microsoft.SqlServer.Management.Smo.BackupDeviceItem') ($bakFullPath, 'File')
+  $rs.Devices.Add($bdi)
+  $fl = $rs.ReadFileList($srv)
+  $logname = $fl.logicalname 
 
-Write-Host "Restore database $DbName..." -NoNewline
+  Write-Host "Restore database $DbName..." -NoNewline
 
-# If the database exists then drop it otherwise Restore-SqlDatabase may fail if connections are open to it
-if ($null -ne $srv.Databases[$DbName])
-{
-    $srv.KillAllProcesses($DbName)
-    $srv.KillDatabase($DbName)
+  # If the database exists then drop it otherwise Restore-SqlDatabase may fail if connections are open to it
+  if ($null -ne $srv.Databases[$DbName])
+  {
+      $srv.KillAllProcesses($DbName)
+      $srv.KillDatabase($DbName)
+  }
+
+  $RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($logname[0], "$dbDataPath\MotusTesting.mdf")
+  $RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($logname[1], "$dbDataPath\MotusTesting_Log.ldf")
+  Restore-SqlDatabase -ServerInstance localhost  -Credential $cred -Database "MotusTesting" -BackupFile "$DbName.bak" -RelocateFile @($RelocateData,$RelocateLog) -RestoreAction Database -ErrorAction Ignore -ReplaceDatabase
+
+  Write-Host "[OK]"
 }
-
-$RelocateData = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($logname[0], "$dbDataPath\MotusTesting.mdf")
-$RelocateLog = New-Object Microsoft.SqlServer.Management.Smo.RelocateFile($logname[1], "$dbDataPath\MotusTesting_Log.ldf")
-Restore-SqlDatabase -ServerInstance localhost  -Credential $cred -Database "MotusTesting" -BackupFile "$DbName.bak" -RelocateFile @($RelocateData,$RelocateLog) -RestoreAction Database -ErrorAction Ignore -ReplaceDatabase
-
-Write-Host "[OK]"
